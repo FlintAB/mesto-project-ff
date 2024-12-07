69b6f67b9e5b45b139f52661600f67b85742cde2
Aleksey Baranovskiy <lari.bro@yandex.ru>
2024-12-07 16:16:43 +0300

    Реализация 6-го модуля

---
+++ src/scripts/card.js
@@ -0,0 +1,48 @@
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content; 
+
+// Функция создания карточки
+function createCard(cardData, deleteCard, likeCard, openImage) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  cardElement
+    .querySelector('.card__delete-button')
+    .addEventListener('click', function() {
+      deleteCard(cardElement);
+    });
+
+  cardElement
+    .querySelector('.card__like-button')
+    .addEventListener('click', likeCard);
+
+  cardImage.addEventListener('click', function() {
+    openImage(cardData);
+  });
+
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(item) {
+  item.remove();
+}
+
+// Вывести карточки на страницу
+function addCard(parent, card) {
+  parent.append(card);
+}
+
+// Функция лайка карточки
+function likeCard(evt) {
+  if (evt.target.classList.contains('card__like-button')){
+    evt.target.classList.toggle('card__like-button_is-active');
+  } 
+}
+
+export {createCard, deleteCard, addCard, likeCard};
--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -23,25 +23,4 @@ export const initialCards = [
      name: "Байкал",
      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
    }
-];
-
-
-
-//Функция создания карточки
-export function createCard(cardData, deleteCard) {
-  //Темплейт карточки
-  const cardTemplate = document.querySelector('#card-template').content;
-  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  const cardImage = cardElement.querySelector('.card__image');
-
-  cardElement.querySelector('.card__title').textContent = cardData.name;
-  cardImage.src = cardData.link;
-  cardImage.alt = cardData.name;
-
-  const deleteButton = cardElement.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', () => deleteCard(cardElement));
-
-  return cardElement;
-}
-
-// описаны функции для работы с карточками: функция создания карточки, функции-обработчики событий удаления и лайка карточки;
+ ];
